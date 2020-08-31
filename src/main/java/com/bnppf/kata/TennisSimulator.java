2161d8c36228b09ed7295f08370dc5d670c30071
Priyal Parikh <parikhpriyal91@yahoo.com>
2020-08-31 21:59:06 +0530

    Add: Continue playing game till winner is identified

--- src/main/java/com/bnppf/kata/TennisSimulator.java
+++ src/main/java/com/bnppf/kata/TennisSimulator.java
@@ -1,18 +1,38 @@
 package com.bnppf.kata;
 
+import com.bnppf.kata.constants.TennisConstants;
 import com.bnppf.kata.entities.TennisPlayer;
+import com.bnppf.kata.exceptions.TennisException;
 import com.bnppf.kata.game.Tennis;
 import com.bnppf.kata.interfaces.TennisInterface;
+import org.apache.log4j.Logger;
 
 import java.util.Scanner;
 
 public class TennisSimulator {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final Logger logger = Logger.getLogger(TennisSimulator.class);
 
     public static void main(String[] args) {
         TennisInterface tennis = startGameWithTwoPlayers();
-        System.out.println("Starting Game Score :" + tennis.getScore());
+        System.out.println("Start New Game!");
+
+        while (!tennis.getScore().contains("Winner")) {
+            System.out.print("Enter point winning player:");
+            String pointScoredByPlayer = scanner.nextLine();
+            try {
+                tennis.increasePlayerScore(pointScoredByPlayer);
+            } catch (TennisException e) {
+                logger.error(TennisConstants.TXT_INVALID_PLAYER + e.getMessage());
+                System.out.println("Exception occurred: " + e.getMessage());
+            }
+            System.out.println("*****************************************************");
+            System.out.println(" " + tennis.getScore());
+            System.out.println("*****************************************************");
+        }
+
+        System.out.println("Game over!");
         scanner.close();
     }
 
